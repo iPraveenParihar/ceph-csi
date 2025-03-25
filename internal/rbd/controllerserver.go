72a15591fdb6458579da93fc33d6cd601ec13756
Nikhil-Ladha <nikhilladha1999@gmail.com>
2025-03-25 12:30:32 +0530

    rbd: delete group only if its primary
    delete the group only if it is primary, and when the
    group is secondary only delete metadata(OMAP) for the
    volumegroup.
    
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>
    (cherry picked from commit 7bb9b28c9ef040e336e42928ccaf3b71a1352c1e)
    (cherry picked from commit aefed11fb435eebfa6401bf117aa318359b08ae0)

--- internal/rbd/controllerserver.go
+++ internal/rbd/controllerserver.go
@@ -25,6 +25,7 @@ import (
 
 	csicommon "github.com/ceph/ceph-csi/internal/csi-common"
 	rbderrors "github.com/ceph/ceph-csi/internal/rbd/errors"
+	"github.com/ceph/ceph-csi/internal/rbd/types"
 	"github.com/ceph/ceph-csi/internal/util"
 	"github.com/ceph/ceph-csi/internal/util/k8s"
 	"github.com/ceph/ceph-csi/internal/util/log"
@@ -966,6 +967,9 @@ func (cs *ControllerServer) DeleteVolume(
 		return nil, status.Error(codes.InvalidArgument, "empty volume ID in request")
 	}
 
+	mgr := NewManager(cs.Driver.GetInstanceID(), nil, req.GetSecrets())
+	defer mgr.Destroy(ctx)
+
 	cr, err := util.NewUserCredentialsWithMigration(req.GetSecrets())
 	if err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
@@ -1020,12 +1024,12 @@ func (cs *ControllerServer) DeleteVolume(
 	}
 	defer cs.VolumeLocks.Release(rbdVol.RequestName)
 
-	return cleanupRBDImage(ctx, rbdVol, cr)
+	return cleanupRBDImage(ctx, rbdVol, cr, mgr)
 }
 
 // cleanupRBDImage removes the rbd image and OMAP metadata associated with it.
-func cleanupRBDImage(ctx context.Context,
-	rbdVol *rbdVolume, cr *util.Credentials,
+func cleanupRBDImage(ctx context.Context, rbdVol *rbdVolume,
+	cr *util.Credentials, mgr types.Manager,
 ) (*csi.DeleteVolumeResponse, error) {
 	rm, err := rbdVol.ToMirror()
 	if err != nil {
@@ -1090,6 +1094,21 @@ func cleanupRBDImage(ctx context.Context,
 		log.ErrorLog(ctx, "rbd %s is still being used", rbdVol)
 
 		return nil, status.Errorf(codes.Aborted, "rbd %s is still being used", rbdVol.RbdImageName)
+	}
+
+	// check if image is part of a group
+	groupId, err := rbdVol.GetVolumeGroupID(ctx, mgr)
+	if err != nil && !errors.Is(err, rbderrors.ErrGroupNotFound) {
+		log.ErrorLog(ctx, err.Error())
+
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if groupId != "" {
+		err = fmt.Errorf("image %q is a part of volume group %q", rbdVol, groupId)
+		log.ErrorLog(ctx, err.Error())
+
+		return nil, status.Error(codes.Aborted, err.Error())
 	}
 
 	// delete the temporary rbd image created as part of volume clone during

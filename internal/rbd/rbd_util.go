01870d5e911ce41f229c179f6b8a9b0245441980
Praveen M <m.praveen@ibm.com>
2026-09-09 16:16:47 +0530

    cleanup: release copied rbd connections to avoid leaks
    Add the missing deferred Destroy() in each.
    
    Signed-off-by: Praveen M <m.praveen@ibm.com>
    (cherry picked from commit 881115cdbe466762ae373897cc65658f36d22b32)

--- internal/rbd/rbd_util.go
+++ internal/rbd/rbd_util.go
@@ -1957,6 +1957,9 @@ func (ri *rbdImage) getParent() (*rbdImage, error) {
 
 	err = parentImage.getImageInfo()
 	if err != nil {
+		parentImage.conn.Destroy()
+		parentImage.conn = nil
+
 		return nil, err
 	}
 
